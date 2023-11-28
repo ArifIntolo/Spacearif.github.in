658c3c483298b3f0864e03eb4500e119f05e8e4a
ArifIntolo <114798352+ArifIntolo@users.noreply.github.com>
2023-11-28 18:07:57 +0530

    Update main.js
    updated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,234 @@
+// Functions for each action-command.
+
+function help(){
+  return "<h2><span style=\"color:#eb926d;\">Help:</span></h2><table>\
+  <tr>\
+    <td>all</td>\
+    <td>Give me the complete picture</td>\
+  </tr>\
+  <tr>\
+    <td>about</td>\
+    <td>Everything you ever wanted to know about Mohammed Arif</td>\
+  </tr>\
+  <tr>\
+    <td>contact</td>\
+    <td>How to get in touch with Mohammed Arif</td>\
+  </tr>\
+  <tr>\
+    <td>links</td>\
+    <td>See what links Mohammed Arif wants you to click on</td>\
+  </tr>\
+  <tr>\
+    <td>skills</td>\
+    <td>Get to know how good Mohammed Arif is at stuff</td>\
+  </tr>\
+  <tr>\
+    <td>cv</td>\
+    <td>Download Mohammed Arif's CV</td>\
+  </tr>\
+  <tr>\
+    <td>rickroll</td>\
+    <td>Never Gonna Give You Up</td>\
+  </tr></table>";
+}
+
+function contact(){
+  return "<h2><span style=\"color:#cc6666;\">Contact:</span></h2><table>\
+  <tr>\
+    <td>Email</td>\
+    <td><a href=\"mailto:arif.ec08@gmail.com\">arif.ec08@gmail.com</a></td>\
+  </tr>\
+  <tr>\
+    <td>Telephone</td>\
+    <td><a href=\"tel:+91-94249-96247\">+91-94249-96247</a></td>\
+  </tr>\
+  <tr>\
+    <td>Address</td>\
+    <td>Mohammed Arif</td>\
+  </tr>\
+  <tr>\
+    <td> </td>\
+    <td>205 Autumn Street</td>\
+  </tr>\
+  <tr>\
+    <td> </td>\
+    <td>Khamaria, Jabalpur</td>\
+  </tr>\
+  <tr>\
+    <td> </td>\
+    <td>Madhya Pradesh</td>\
+  </tr>\
+  <tr>\
+    <td> </td>\
+    <td>482005</td>\
+  </tr></table>";
+}
+
+function skills(){
+  return "<h2><span style=\"color:#81a2be;\">Skills:</span></h2><table>\
+  <tr>\
+    <td>Python</td>\
+    <td>▰▰▰▰▰▰▱▱▱▱ 60%</td>\
+  </tr>\
+  <tr>\
+    <td>HTML</td>\
+    <td>▰▰▰▰▰▱▱▱▱▱ 50%</td>\
+  </tr>\
+  <tr>\
+    <td>CSS</td>\
+    <td>▰▰▰▰▰▰▱▱▱▱ 60%</td>\
+  </tr>\
+  <tr>\
+    <td>Bootstrap</td>\
+    <td>▰▰▰▰▰▰▰▱▱▱ 70%</td>\
+  </tr>\
+  <tr>\
+    <td>Photoshop</td>\
+    <td>▰▰▰▰▰▰▱▱▱▱ 60%</td>\
+  </tr>\
+  <tr>\
+    <td>Rickrolling</td>\
+    <td>▰▰▰▰▰▰▰▰▰▰ 100%</td>\
+  </tr></table>";
+}
+
+function about(){
+  return "<p>Hard-working and passionate Embedded Engineer with teaching experience of 12+ years in creating positive environments in which teenagers can learn and grow. Incorporates interdisciplinary knowledge into the classroom to engage students of all learning orientations</p>";
+}
+
+function cv(){
+  return "<span style=\"color:#8abeb7;\"><h2>CV:</h2></span><p>\tDownload from <a href=\"src/cv.pdf\" target=\"_blank\" style=\"text-decoration: underline;\">here</a>.</p>";
+}
+
+function credits(){
+  return "<p>Built by <a href=\"https://www.github.com/CedArctic\" target=\"_blank\"><i class=\"fab fa-github\"></i> CedArctic</a></p>";
+}
+
+function links(){
+  return "<span style=\"color: #b5bd68;\"><h2>Links:</h2></span><ul>\
+  <li><a href=\"https://www.linkedin.com\" target=\"_blank\"><i class=\"fab fa-linkedin\"></i> LinkedIn</a></li>\
+  <li><a href=\"https://www.github.com\" target=\"_blank\"><i class=\"fab fa-github\"></i> Github</a></li>\
+  <li><a href=\"https://www.twitter.com\" target=\"_blank\"><i class=\"fab fa-twitter\"></i> Twitter</a></li>\
+  <li><a href=\"https://www.facebook.com\" target=\"_blank\"><i class=\"fab fa-facebook\"></i> Facebook</a></li>\
+  </ul>";
+}
+
+function rickroll(){
+  return "<p>We're no strangers to love<br>\
+    You know the rules and so do I<br>\
+    A full commitment's what I'm thinking of<br>\
+    You wouldn't get this from any other guy<br>\
+    <br>\
+    I just wanna tell you how I'm feeling<br>\
+    Gotta make you understand<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    We've known each other for so long<br>\
+    Your heart's been aching, but<br>\
+    You're too shy to say it<br>\
+    Inside, we both know what's been going on<br>\
+    We know the game and we're gonna play it<br>\
+    <br>\
+    And if you ask me how I'm feeling<br>\
+    Don't tell me you're too blind to see<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    (Ooh, give you up)<br>\
+    (Ooh, give you up)<br>\
+    Never gonna give, never gonna give<br>\
+    (Give you up)<br>\
+    Never gonna give, never gonna give<br>\
+    (Give you up)<br>\
+    <br>\
+    We've known each other for so long<br>\
+    Your heart's been aching, but<br>\
+    You're too shy to say it<br>\
+    Inside, we both know what's been going on<br>\
+    We know the game and we're gonna play it<br>\
+    <br>\
+    I just wanna tell you how I'm feeling<br>\
+    Gotta make you understand<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you</p>";
+}
+
+// Main Function
+function commandProcessor(e){
+
+  //Check if the enter key is pressed
+  if(e.keyCode == 13){
+
+    //Clear the area where info will be printed
+    document.getElementById('injected').innerHTML= "";
+
+    //Get user input
+    var txtInput = document.getElementById('txtBox').value;
+
+    //Select what info to print according to command
+    if(txtInput == "help"){
+      document.getElementById('injected').innerHTML=help();
+    }else if (txtInput=="all") {
+      document.getElementById('injected').innerHTML=about() + "\n\n\n" + skills() + "\n\n\n" + links() + "\n\n\n" + contact() + "\n\n\n" + cv();
+    }else if (txtInput == "about") {
+      document.getElementById('injected').innerHTML=about();
+    }else if (txtInput == "contact") {
+      document.getElementById('injected').innerHTML=contact();
+    }else if (txtInput == "cv") {
+      document.getElementById('injected').innerHTML=cv();
+    }else if (txtInput=="skills") {
+      document.getElementById('injected').innerHTML=skills();
+    }else if (txtInput=="links") {
+      document.getElementById('injected').innerHTML=links();
+    }else if (txtInput == "rickroll") {
+      var win = window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank');
+      win.focus();
+      document.getElementById('injected').innerHTML=rickroll();
+    }else if (txtInput == "credits") {
+      document.getElementById('injected').innerHTML=credits();
+    }else{
+      document.getElementById('injected').innerHTML = help();
+    }
+
+    //Clear input text box
+    document.getElementById('txtBox').value= "";
+  }
+}
 
